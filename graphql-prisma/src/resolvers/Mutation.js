7c9129f63831948c2887c3e2e5c2615bc50a9d62
imran <engr.imrannaqvi@gmail.com>
2022-03-01 12:31:53 +0500

    before wsl2

--- graphql-prisma/src/resolvers/Mutation.js
+++ graphql-prisma/src/resolvers/Mutation.js
@@ -1,14 +1,52 @@
-import uuidv4 from 'uuid/v4';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import getUserId  from '../utils/getUserId';
 
 const Mutation = {
   async createUser(parent, args, { db, prisma }, info){
+    if(args.data.password.length < 8) {
+      throw new Error("Password must be 8 characters or longer")
+    }
     const emailExists = await prisma.exists.User({ email: args.data.email});
 
     if(emailExists){
       throw new Error("Email already exists");
     }
+    
+    const password = await bcrypt.hash(args.data.password, 10);
+    const user = prisma.mutation.createUser({
+        data: {
+        ...args.data,
+        password
+      }
+    })
 
-    return prisma.mutation.createUser({data: args.data}, info)
+    return {
+      user,
+      token: jwt.sign({ userId: user.id }, 'thisissecret')
+    }
+  },
+  async login(parent, args, {prisma}, info){
+    const user = await prisma.query.user({
+      where: {
+        email: args.data.email
+      }
+    })
+
+    if (!user) {
+      throw new Error('Unable to find user')
+    }
+
+    const isMatch = await bcrypt.compare(args.data.password, user.password)
+    if (!isMatch) {
+      throw new Error('Password doesnot match')
+    }
+
+    return{
+      user,
+      token: jwt.sign({userId: user.id}, 'thisissecret')
+    }
+
   },
   deleteUser(parent, args, { prisma }, info){
     return prisma.mutation.deleteUser({where: {
@@ -23,12 +61,13 @@ const Mutation = {
       }
     }, info)
   },
-async createPost(parent, args, { prisma, pubsub }, info){  
+async createPost(parent, args, { prisma, request }, info){  
+  const userId = getUserId(request)
   const newPost = await prisma.mutation.createPost({data: {
     ...args.data,
     author: {
       connect: {
-        id: args.data.author
+        id: userId
       }
     }
   }}, info)
